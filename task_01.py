40578b617f18f688007e8939d6b8ec097d46b87a
Vagrant User <mr.nikovaccaro@gmail.com>
2015-10-03 12:31:26 +0000

    error

--- task_01.py
+++ task_01.py
@@ -6,5 +6,9 @@
 ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium',
             'Beryllium', 'Boron', 'Carbon')
 
-OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division'
-                    'Addition', 'Subtraction']
+OPERATIONS_ORDER = ['Parentheses',
+                    'Exponents',
+                    'Multiplication',
+                    'Division'
+                    'Addition',
+                    'Subtraction']
